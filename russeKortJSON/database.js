aa3dc57e6464cf7320627cac995bbd0b6373041d
Andersgrav <113419669+Andersgrav@users.noreply.github.com>
2023-05-27 22:57:28 +0200

    Delete russeKortJSON directory

--- russeKortJSON/database.js
+++
@@ -1,8 +0,0 @@
-let russeKort = {
-    "personer": [
-        { "navn": "Marcus", "skole": "Prekestolen GK", "adr": "adr: Plantasjen", "tlf": "tlf: nokia", "sitat1": "Mængo", "sitat2": "NONONO" },
-        { "navn": "Jonas", "skole": "Romerrike", "adr": "adr: Roma", "tlf": "tlf: 911", "sitat1": "Roma ruler", "sitat2": "#gladiator" },
-        { "navn": "Jens", "skole": "Nesbru", "adr": "adr: Fekjan", "tlf": "tlf: Ikke ring", "sitat1": "Mamma liker meg", "sitat2": "#Yolo" },
-        { "navn": "Tor", "skole": "Sandvika", "adr": "adr: Mækkern", "tlf": "tlf: Peppes", "sitat1": "mat er digg", "sitat2": "#foodlover" },
-    ],
-}
